b5a676f899061b11601f2870bcbe4f3fd0150126
j5a6i7n8i9s0h <21962504@student.uwa.edu.au>
2018-10-24 23:16:45 +0800

    multiplication - needs work but i think it works

--- matrix_mul.c
+++ matrix_mul.c
@@ -5,8 +5,8 @@
 
 
 typedef struct {
-	int i; 
-	int j;
+	int row; 
+	int col;
 	float val; 
 }MatrixMarket;
 
@@ -21,9 +21,7 @@ int getLineCount(FILE*file)
 {
 	int lines = 0;
 	while(!feof(file))
-	{
 		lines = fgetc(file)=='\n'? lines+1:lines; 
-	}
 	return lines; 
 }
 
@@ -38,21 +36,66 @@ int createMatrixFromFile(Matrix*matrix, FILE*file)
 	MatrixMarket temp;
 	matrix->market = (MatrixMarket*)malloc(count*sizeof(MatrixMarket)); 
 	if(matrix->market==NULL)
+	{
+		fprintf(stderr,"Failure to allocate memory\n");
 		return EXIT_FAILURE;
+	}
 	matrix->count=count;
 	fseek(file, 0, SEEK_SET);
 	while(fgets(buff, BUFSIZ, file)!=NULL)
 	{
 		sscanf(buff,"%d %d %f", &row, &col, &val);
-		if(matrix->num_cols<col)
-			matrix->num_cols=col;
-		if(matrix->num_rows<row)
-			matrix->num_rows=row;
-		temp.i = row;
-		temp.j = col;
+		matrix->num_cols=matrix->num_cols<col?col:matrix->num_cols;
+		matrix->num_rows= matrix->num_rows<row?row:matrix->num_rows;
+		temp.row = row;
+		temp.col = col;
 		temp.val = val;
 		matrix->market[i++]=temp;
 	}
+	return EXIT_SUCCESS;
+}
+
+int matrix_multiplication(Matrix*matrix_a, Matrix*matrix_b, Matrix*matrix_c)
+{
+	MatrixMarket *mat_a = matrix_a->market;
+	MatrixMarket *mat_b = matrix_b->market;
+	MatrixMarket *mat_c = (MatrixMarket*)malloc(matrix_a->count*sizeof(MatrixMarket)); 
+	if(mat_c==NULL)
+	{
+		fprintf(stderr,"Failed to allocate memory\n");
+		return EXIT_FAILURE;
+	}
+	int insert=0;
+	for(int apos=0;apos<matrix_a->count;)
+	{
+		int row_to_consider =  mat_a[apos].row;
+		for(int bpos =0 ; bpos<matrix_b->count;)
+		{
+			int col_to_consider = mat_b[bpos].col;
+			int temp_a = apos;
+			int temp_b = bpos;
+
+			float val = 0.0;
+			while(temp_a < matrix_a->count && mat_a[temp_a].row == row_to_consider && temp_b < matrix_b->count && mat_b[temp_b].col == col_to_consider)
+			{
+				if(mat_a[temp_a].col < mat_b[temp_b].row) temp_a++;
+				else if(mat_a[temp_a].col > mat_b[temp_b].row) temp_b++;
+				else val += mat_a[temp_a++].val*mat_b[temp_b++].val;
+			}
+			if(val!=0)
+			{
+				MatrixMarket temp; 
+				temp.col = col_to_consider;
+				temp.row = row_to_consider;
+				temp.val = val;
+				mat_c[insert++] = temp;
+			}
+			while(bpos < matrix_b->count && mat_b[bpos].col == col_to_consider) bpos++;	
+		}
+		while(apos < matrix_a->count && mat_a[apos].row == row_to_consider) apos++;
+	}
+	matrix_c->market=mat_c;
+	matrix_c->count=insert;
 	return EXIT_SUCCESS;
 }
 
@@ -60,19 +103,19 @@ int main(int argc, char* argv[])
 {
 	FILE*file;
 	FILE*file_2;
-	bool fexists;
+	bool f_exists;
 	if(argc==3)
 	{
 		file = fopen(argv[1], "r");
 		file_2 = fopen(argv[2],"r");
-		fexists = file!=NULL && file_2!=NULL;
+		f_exists = file!=NULL && file_2!=NULL;
 	}
 	else if(argc==2)
 	{
 		file = fopen(argv[1], "r");
-		fexists = file!=NULL;
+		f_exists = file!=NULL;
 	}
-	if(fexists)
+	if(f_exists)
 	{
 		
 		Matrix matrix_a;
@@ -110,12 +153,25 @@ int main(int argc, char* argv[])
 		printf("%d valid entries \n", matrix_b.count);
 		printf("%d x %d matrix created \n", matrix_b.num_rows, matrix_b.num_cols);
 
-		Matrix matrix_b;
+		Matrix matrix_c;
 		if(matrix_a.num_cols!=matrix_b.num_rows)
 		{
 			fprintf(stderr, "Invalid dimensions to multiply\n");
 			return EXIT_FAILURE;
 		}
+		matrix_c.num_rows = matrix_a.num_rows;
+		matrix_c.num_cols = matrix_b.num_cols;
+		//matrix_c.count = matrix_a.count;
+		if(matrix_multiplication(&matrix_a,&matrix_b,&matrix_c)==EXIT_FAILURE)
+		{	
+			return EXIT_FAILURE;
+		}
+		for(int i=0;i<matrix_c.count;i++)
+		{
+			printf("%f \n",matrix_c.market[i].val);
+		}
+		printf("%d x %d matrix created \n", matrix_c.num_rows, matrix_c.num_cols);
+
 	}
 	else
 	{
@@ -123,4 +179,5 @@ int main(int argc, char* argv[])
 	}
 	fclose(file);
 	fclose(file_2);
+	return 0;
 }

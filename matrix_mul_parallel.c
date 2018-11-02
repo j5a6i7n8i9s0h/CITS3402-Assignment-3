c102d2cd14d4dbc19c7ed3e2e712abae3f21be80
j5a6i7n8i9s0h <21962504@student.uwa.edu.au>
2018-11-02 17:53:06 +0800

    USE parallel.c , ignore refactor

--- matrix_mul_parallel.c
+++ matrix_mul_parallel.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <omp.h>
 #include <mpi.h>
+#include <time.h>
 
 #define MASTER 0
 #define FROM_MASTER 1 /* setting a message type */
@@ -16,8 +17,6 @@ typedef struct {
 } MatrixMarket;
 
 typedef struct {
-	int num_rows; 
-	int num_cols;
 	int count; 
 	MatrixMarket *market;
 } Matrix;
@@ -35,102 +34,32 @@ void clearMatrixFromMemory(Matrix mat)
 	free(mat.market);
 }
 
-void createMatrixFromFile(Matrix *matrix, FILE *file)
+void createMatrixFromFile(Matrix *matrix, FILE *file, int count)
 {
 	char buff[BUFSIZ];
 	int row;
 	int col; 
 	float val;
 	int i = 0;
-	int count = getLineCount(file);
 	MatrixMarket temp;
 	matrix->market = (MatrixMarket*)malloc(count*sizeof(MatrixMarket)); 
-
 	if(matrix->market == NULL)
 	{
 		fprintf(stderr,"Failure to allocate memory\n");
+		MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
 		exit(EXIT_FAILURE);
 	}
-
 	matrix->count = count;
 	fseek(file, 0, SEEK_SET);
-
 	while(fgets(buff, BUFSIZ, file) != NULL)
 	{
 		sscanf(buff,"%d %d %f", &row, &col, &val);
-		matrix->num_cols = (matrix->num_cols < col) ? col : matrix->num_cols;
-		matrix->num_rows = (matrix->num_rows < row) ? row : matrix->num_rows;
 		temp.row = row;
 		temp.col = col;
 		temp.val = val;
 		matrix->market[i++] = temp;
 	}
 }
-
-// void matrix_multiplication(Matrix *matrix_a, Matrix *matrix_b, Matrix *matrix_c)
-// {
-// 	MatrixMarket *mat_a = matrix_a->market;
-// 	MatrixMarket *mat_b = matrix_b->market;
-// 	MatrixMarket *mat_c = (MatrixMarket*)malloc(matrix_a->count*sizeof(MatrixMarket)); 
-
-// 	if(mat_c == NULL)
-// 	{
-// 		fprintf(stderr,"Failed to allocate memory\n");
-// 		exit(EXIT_FAILURE);
-// 	}
-
-// 	int apos, row_to_consider, insert = 0;
-
-// 	#pragma omp parallel for private(apos) collapse(2) shared(insert, row_to_consider)
-// 	for(apos = 0; apos < matrix_a->count; )
-// 	{
-// 		for(int bpos = 0; bpos < matrix_b->count; )
-// 		{
-// 			row_to_consider =  mat_a[apos].row;
-// 			int col_to_consider = mat_b[bpos].col;
-// 			int temp_a = apos;
-// 			int temp_b = bpos;
-
-// 			float val = 0.0;
-// 			while(temp_a < matrix_a->count
-// 				&& mat_a[temp_a].row == row_to_consider
-// 				&& temp_b < matrix_b->count
-// 				&& mat_b[temp_b].col == col_to_consider)
-// 			{
-// 				if(mat_a[temp_a].col < mat_b[temp_b].row) temp_a++;
-// 				else if(mat_a[temp_a].col > mat_b[temp_b].row) temp_b++;
-// 				else val += (mat_a[temp_a++].val * mat_b[temp_b++].val);
-// 			}
-// 			if(val != 0)
-// 			{
-// 				MatrixMarket temp; 
-// 				bool newPos = true;
-
-// 				for(int i = 0; i < insert; i++)
-// 				{
-// 					if(mat_c[i].col == col_to_consider && mat_c[i].row == row_to_consider)
-// 					{
-// 						newPos = false;
-// 						mat_c[i].val += val;
-// 					}
-// 				}
-
-// 				if(newPos)
-// 				{
-// 					temp.col = col_to_consider;
-// 					temp.row = row_to_consider;
-// 					temp.val = val;
-// 					mat_c[insert++] = temp;
-// 				}
-// 			}
-// 			while(bpos < matrix_b->count && mat_b[bpos].col == col_to_consider) bpos++;	
-// 		}
-// 		while(apos < matrix_a->count && mat_a[apos].row == row_to_consider) apos++;
-// 	}
-
-// 	matrix_c->market = mat_c;
-// 	matrix_c->count = insert;
-// }
 
 int main(int argc, char* argv[])
 {
@@ -147,24 +76,22 @@ int main(int argc, char* argv[])
 	FILE *file, *file_2;
 	bool f_exists;
 
-	if(argc == 3)
-	{
-		file = fopen(argv[1], "r");
-		file_2 = fopen(argv[2],"r");
-		f_exists = file != NULL && file_2 != NULL;
-	}
+	file = fopen(argv[1], "r");
+	file_2 = fopen(argv[2],"r");
+	f_exists = file != NULL && file_2 != NULL;
 
 	if(!f_exists)
 	{
-		fprintf(stderr, "Invalid file provided \n");
-
+		fprintf(stderr, "Invalid file(s) provided \n");
 		fclose(file);
 		fclose(file_2);
-
-		//MPI_Finalize();
 		return EXIT_FAILURE;
 
 	}
+	
+	clock_t begin = clock();
+
+
 	int file_1_lines = getLineCount(file);
 	fseek(file, 0, SEEK_SET);
 	int file_2_lines = getLineCount(file_2);
@@ -206,24 +133,13 @@ int main(int argc, char* argv[])
 	}
 	if(taskid == MASTER)
 	{
-		printf("Master doing stuff\n");
 		Matrix matrix_a;
 		Matrix matrix_b;
 		Matrix matrix_c;
-		matrix_a.num_rows = 0;
-		matrix_a.num_cols = 0;
-		matrix_c.count =0;
+		matrix_c.count = 0;
 
-		createMatrixFromFile(&matrix_a, file);
-
-		printf("%d valid entries \n", matrix_a.count);
-		printf("%d x %d matrix created \n", matrix_a.num_rows, matrix_a.num_cols);
-
-		matrix_b.num_rows = 0;
-		matrix_b.num_cols = 0;
-
-		createMatrixFromFile(&matrix_b, file_2);
-
+		createMatrixFromFile(&matrix_a, file, file_1_lines);
+		createMatrixFromFile(&matrix_b, file_2, file_2_lines);
 		mat_a = matrix_a.market;
 		mat_b = matrix_b.market;
 		averow = matrix_a.count/numworkers;
@@ -246,7 +162,6 @@ int main(int argc, char* argv[])
 			MPI_Recv(&offset, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
 			MPI_Recv(&rows, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
 			MPI_Recv(&insert, 1, MPI_INT, source, mtype, MPI_COMM_WORLD,&status);
-			printf("breaks here :  taskid =%d offset=%d insert=%d rows=%d\n",source,offset, insert,rows);
 			MPI_Recv(mat_c_temp, insert, mpi_matrix_market, source, mtype, MPI_COMM_WORLD, &status);
 			if(insert>0){
 				for(int in=0;in<insert;in++)
@@ -269,11 +184,13 @@ int main(int argc, char* argv[])
 			printf("Received results from task %d\n",source);
 		}
 		matrix_c.market=mat_c;
-		for(int lp=0;lp<matrix_c.count;lp++)
+		for(i=0;i<matrix_c.count;i++)
 		{
-			printf("%f %d %d market \n",matrix_c.market[lp].val, matrix_c.market[lp].row, matrix_c.market[lp].col);
+			printf("%d %d %f \n", matrix_c.market[i].row, matrix_c.market[i].col, matrix_c.market[i].val);
 		}
-		printf("TOTAL %d    total lines %d \n",matrix_c.count, max_lines);
+
+		clock_t end = clock();
+		printf("Time: %f ms \n", (float)(end-begin)*1000/CLOCKS_PER_SEC);
 	}
 
 	if(taskid > MASTER)
@@ -284,8 +201,7 @@ int main(int argc, char* argv[])
 		MPI_Recv(mat_a, rows, mpi_matrix_market, MASTER, mtype, MPI_COMM_WORLD, &status);
 		MPI_Recv(mat_b, file_2_lines, mpi_matrix_market, MASTER, mtype, MPI_COMM_WORLD, &status);
 		int apos, row_to_consider;
-		insert = 0;
-
+		insert = 0; // number of elements to be inserted
 		for(apos = 0; apos < rows; )
 		{
 			row_to_consider =  mat_a[apos].row;
@@ -338,6 +254,9 @@ int main(int argc, char* argv[])
 	MPI_Finalize();
 	fclose(file);
 	fclose(file_2);
-
+	free(mat_c);
+	free(mat_a);
+	free(mat_b);
+	free(mat_c_temp);
 	return EXIT_SUCCESS;
 }
